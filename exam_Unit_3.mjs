7e3af96ba6ef14e1cc7cff6fe50d848f3893ba61
aleksteig <aleksteigen@gmail.com>
2025-03-22 18:10:02 +0100

    got a clue and made a table, still do not get the first challenge

--- exam_Unit_3.mjs
+++ exam_Unit_3.mjs
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-async function interactionWithAPI(endpoint, method, body = null) {
+async function interactionWithAPI(endpoint, method, body = null, expectHtml = false) {
     const response = await fetch(`https://alchemy-kd0l.onrender.com${endpoint}`, {
         method,
         headers: {
@@ -13,19 +13,55 @@ async function interactionWithAPI(endpoint, method, body = null) {
         throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
+    if(expectHtml){
+        return await response.text();
+    }
+
     return await response.json();
 }
 
 const PLAYER_NAME = "aleksandte@uia.no";
-const USER_ANSWER1 = 71257;
+const USER_ANSWER1 = "34213";
+
+const symbols = {
+    "symbol ☉": {
+        "planar body": "sun",
+        "periodic name": "Au",
+        "periodic number": 79,
+        "periodic element": "gold",
+    },
+    "symbol ☿": {
+        "planar body": "mercury",
+        "periodic name": "Hg",
+        "periodic number": 80,
+        "periodic element": "quicksilver/mercury",
+    },
+    "symbol ☽": {
+        "planar body": "moon",
+        "periodic name": "Ag",
+        "periodic number": 47,
+        "periodic element": "silver",
+    },
+    "symbol ♂": {
+        "planar body": "mars",
+        "periodic name": "Fe",
+        "periodic number": 26,
+        "periodic element": "iron",
+    },
+    
+}
 
 const start = await interactionWithAPI(`/start?player=${PLAYER_NAME}`, 'GET');
 
 //console.log(start);
 
+const clue1 = await interactionWithAPI(`/clue?player=${PLAYER_NAME}`, 'GET', null, true);
+
+//console.log(clue1);
+
 const answer1 = await interactionWithAPI(`/answer`, 'POST', {
-    'player': PLAYER_NAME,
-    'answer': USER_ANSWER1,
+    "player": PLAYER_NAME,
+    "answer": USER_ANSWER1,
 });
 
-console.log(answer1);
+//console.log(answer1);
